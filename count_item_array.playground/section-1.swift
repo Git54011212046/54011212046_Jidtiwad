e34c3de87a4c10b1e16119018d815f27c8c2f1c7
Git54011212046 <sky5521200@gmail.com>
2015-01-16 17:03:31 +0700

    array item count
    count item in array

---
+++ count_item_array.playground/section-1.swift
@@ -0,0 +1,9 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let possibleTipsInferred = [0.15, 0.18, 0.20]
+let possibleTipsExplicit: [Double] = [0.15, 0.18, 0.20]
+var numberOfItem = possibleTipsInferred.count
