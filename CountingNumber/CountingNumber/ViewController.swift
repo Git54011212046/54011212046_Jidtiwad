84a3049a644c96d17e400fa586733838e847483a
Git54011212046 <sky5521200@gmail.com>
2015-02-06 17:06:30 +0700

    test last hour
    counting number

---
+++ CountingNumber/CountingNumber/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  CountingNumber
+//
+//  Created by iStudents on 2/6/15.
+//  Copyright (c) 2015 iStudents. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var oneOutlet: UILabel!
+    @IBOutlet weak var twoOutlet: UILabel!
+    @IBOutlet weak var threeOutlet: UILabel!
+    var subCount = 0
+    
+    @IBAction func oneBT(sender: AnyObject) {
+        subCount = subCount + 1
+        oneOutlet.text = "\(subCount)"
+    }
+    @IBAction func twoBT(sender: AnyObject) {
+        subCount = subCount + 2
+        twoOutlet.text = "\(subCount)"
+    }
+    @IBAction func threeBT(sender: AnyObject) {
+        subCount = subCount + 3
+        threeOutlet.text = "\(subCount)"
+    }
+    @IBAction func resetBBT(sender: AnyObject) {
+        oneOutlet.text = "0"
+        twoOutlet.text = "0"
+        threeOutlet.text = "0"
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ counting.swift
@@ -0,0 +1,35 @@
+//
+//  counting.swift
+//  CountingNumber
+//
+//  Created by iStudents on 2/6/15.
+//  Copyright (c) 2015 iStudents. All rights reserved.
+//
+
+import Foundation
+
+class counting {
+    var subCount = 0
+    var result: Int {
+        get {
+            return subCount
+        }
+    }
+    
+    init () {}
+    
+    func oneClick(oneValue: Int) ->Int {
+        subCount = oneValue + 1
+        return subCount
+        
+    }
+    func twoClick() ->Int {
+        subCount = subCount + 2
+        return subCount
+    }
+    func threeClick() ->Int {
+        subCount = subCount + 3
+        return subCount
+    }
+    
+}
